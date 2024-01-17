6fa75c26a79aeccc53d3206da0152d562a763aea
moiz.akhtar <moiz.akhtar@pitb.gov.pk>
2024-01-17 23:15:02 +0500

    Integrate Square

--- app/Http/Controllers/SquarePaymentController.php
+++ app/Http/Controllers/SquarePaymentController.php
@@ -41,6 +41,8 @@ class SquarePaymentController extends Controller
         $status_code = $response->status();
         $response_body = json_decode($response->getBody(), true);
 
+        dd($response_body);
+
         $package->update([
             'payment_status' => 'Paid',
         ]);
