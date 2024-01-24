13147f7d01614b745e467b28f2ad8e9b5a022073
moiz.akhtar <moiz.akhtar@pitb.gov.pk>
2024-01-24 23:30:32 +0500

    Dashboard Changes

--- app/Http/Controllers/SquarePaymentController.php
+++ app/Http/Controllers/SquarePaymentController.php
@@ -107,6 +107,7 @@ class SquarePaymentController extends Controller
 
                 $cc_response = Http::withHeaders($cc_headers)->post($cc_url, $cc_body);
                 $cc_response = json_decode($cc_response->getBody(), true);
+                dd($cc_response);
 
                 $payment->update([
                     'square_customer_id' => $cc_response['customer']['id'],
