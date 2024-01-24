970c0ba964dbe09045cea026e7a00ec71929c7b1
moiz.akhtar <moiz.akhtar@pitb.gov.pk>
2024-01-25 01:09:14 +0500

    square payments

--- app/Http/Controllers/PackageController.php
+++ app/Http/Controllers/PackageController.php
@@ -248,9 +248,9 @@ class PackageController extends Controller
         }
 
         $payments = [];
-        $payments = Payment::where('package_id', $packag->id)->get();
+        $payments = Payment::where('payment_module', 'package')->where('payment_module_id', $packag->id)->get();
         $package_files = [];
-        $package_files = PackageFile::where('payment_module', 'package')->where('payment_module_id', $packag->id)->get();
+        // $package_files = PackageFile::where('payment_module', 'package')->where('payment_module_id', $packag->id)->get();
 
         return Inertia::render('Packages/Show', [
             'packag' => $packag,
