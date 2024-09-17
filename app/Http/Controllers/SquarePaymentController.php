29652477fdc5efaa240f4585a6d3e28d79ade89e
moiz.akhtar <moiz.akhtar@pitb.gov.pk>
2024-09-17 13:38:10 +0500

    testing bulk payment on live

--- app/Http/Controllers/SquarePaymentController.php
+++ app/Http/Controllers/SquarePaymentController.php
@@ -332,64 +332,65 @@ class SquarePaymentController extends Controller
     {
         try {
             $packages = Package::where('package_status_id', 3)->where('payment_status', 'Pending')->get();
+            $packages_sum = $packages->sum('grand_total');
 
-            foreach ($packages as $key => $package) {
-                $this->paymentRules($package);
+            $grand_total = $packages_sum * 100;
+            $grand_total_array = explode(".", $grand_total);
+            $amount = (int) $grand_total_array[0];
 
-                if ($package->payment_status == 'Pending') {
-                    $grand_total = $package->grand_total * 100;
-                    $grand_total_array = explode(".", $grand_total);
-                    $amount = (int) $grand_total_array[0];
+            $headers = [
+                'Authorization' => 'Bearer EAAAEPcP7wW7hp68oZHTLDGY4E7XjEAQWGFzLHVrIFpElBcX6CTDSSkk0UsEKx4e'
+                // 'Authorization' => 'Bearer EAAAFIT1m3W_vYnBwzTr1M2OktU_vMDVT2tTm1OcNIcFSPa1X5oABXlHYx2P4kxN'
+            ];
 
-                    $headers = [
-                        'Authorization' => 'Bearer EAAAEPcP7wW7hp68oZHTLDGY4E7XjEAQWGFzLHVrIFpElBcX6CTDSSkk0UsEKx4e'
-                        // 'Authorization' => 'Bearer EAAAFIT1m3W_vYnBwzTr1M2OktU_vMDVT2tTm1OcNIcFSPa1X5oABXlHYx2P4kxN'
-                    ];
+            // CREATE CUSTOMER
+            $customer_url = 'https://connect.squareupsandbox.com/v2/customers';
+            // $customer_url = 'https://connect.squareup.com/v2/customers';
 
-                    // CREATE CUSTOMER
-                    $customer_url = 'https://connect.squareupsandbox.com/v2/customers';
-                    // $customer_url = 'https://connect.squareup.com/v2/customers';
+            $customer_body = [
+                "company_name" => $packages[0]->customer->name,
+                'idempotency_key' => (string) Str::uuid(),
+            ];
 
-                    $customer_body = [
-                        "company_name" => $package->customer->name,
-                        'idempotency_key' => (string) Str::uuid(),
-                    ];
+            $customer_response = Http::withHeaders($headers)->post($customer_url, $customer_body);
+            $customer_response = json_decode($customer_response->getBody(), true);
 
-                    $customer_response = Http::withHeaders($headers)->post($customer_url, $customer_body);
-                    $customer_response = json_decode($customer_response->getBody(), true);
+            // CREATE CARD
+            $card_url = 'https://connect.squareupsandbox.com/v2/cards';
+            // $card_url = 'https://connect.squareup.com/v2/cards';
 
-                    // CREATE CARD
-                    $card_url = 'https://connect.squareupsandbox.com/v2/cards';
-                    // $card_url = 'https://connect.squareup.com/v2/cards';
+            $card_body = [
+                "card" => [
+                    "cardholder_name" => $packages[0]->customer->name,
+                    "customer_id" => $customer_response['customer']['id']
+                ],
+                'idempotency_key' => (string) Str::uuid(),
+                'source_id' => $request->payment_token,
+            ];
 
-                    $card_body = [
-                        "card" => [
-                            "cardholder_name" => $package->customer->name,
-                            "customer_id" => $customer_response['customer']['id']
-                        ],
-                        'idempotency_key' => (string) Str::uuid(),
-                        'source_id' => $request->payment_token,
-                    ];
+            $card_response = Http::withHeaders($headers)->post($card_url, $card_body);
+            $card_response = json_decode($card_response->getBody(), true);
 
-                    $card_response = Http::withHeaders($headers)->post($card_url, $card_body);
-                    $card_response = json_decode($card_response->getBody(), true);
+            // CREATE PAYMENT
+            $payment_url = 'https://connect.squareupsandbox.com/v2/payments';
+            // $payment_url = 'https://connect.squareup.com/v2/payments';
 
-                    // CREATE PAYMENT
-                    $payment_url = 'https://connect.squareupsandbox.com/v2/payments';
-                    // $payment_url = 'https://connect.squareup.com/v2/payments';
+            $payment_body = [
+                'amount_money' => [
+                    'amount' => $amount,
+                    'currency' => 'USD',
+                ],
+                'idempotency_key' => (string) Str::uuid(),
+                'source_id' => $card_response['card']['id'],
+                'customer_id' => $customer_response['customer']['id'],
+            ];
 
-                    $payment_body = [
-                        'amount_money' => [
-                            'amount' => $amount,
-                            'currency' => 'USD',
-                        ],
-                        'idempotency_key' => (string) Str::uuid(),
-                        'source_id' => $card_response['card']['id'],
-                        'customer_id' => $customer_response['customer']['id'],
-                    ];
+            $payment_response = Http::withHeaders($headers)->post($payment_url, $payment_body);
+            $payment_response = json_decode($payment_response->getBody(), true);
 
-                    $payment_response = Http::withHeaders($headers)->post($payment_url, $payment_body);
-                    $payment_response = json_decode($payment_response->getBody(), true);
+            if ($payment_response['payment']['status'] === 'COMPLETED') {
+
+                foreach ($packages as $key => $package) {
 
                     $data = [
                         'payment_module' => 'package',
@@ -413,18 +414,13 @@ class SquarePaymentController extends Controller
                         'payment_module_id' => $package->id,
                     ], $data);
 
-                    if ($payment_response['payment']['status'] === 'COMPLETED') {
+                    paymentInvoiceForLabel($payment->id);
 
-                        paymentInvoiceForLabel($payment->id);
-
-                        $package->update([
-                            'payment_status' => 'Paid',
-                            'package_status_id' => 5,
-                            'cart' => 0,
-                        ]);
-                    }
-                } else {
-                    abort(403, 'already-paid');
+                    $package->update([
+                        'payment_status' => 'Paid',
+                        'package_status_id' => 5,
+                        'cart' => 0,
+                    ]);
                 }
             }
 
