4255df5bc56d24703f38c260d1df2c97cc4cb882
moiz.akhtar <moiz.akhtar@pitb.gov.pk>
2024-09-28 18:59:02 +0500

    fixes in inquiry chat

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,19 +10,7 @@
     <link rel="icon" type="image/x-icon" href="{{ asset('images/favicon.png') }}">
     <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('css/custom.css?v=1.0') }}">
-
-    @env('production')
-    <script
-        src="https://www.paypal.com/sdk/js?client-id=Ad_mOnLAjPkl17HazcpuehUPrOIEP9rsM90Ta1BRuUSdvAe14-lcWx1ZWjCcESkSrqjJ_xjnogdy4ft6&enable-funding=venmo&currency=USD"
-        data-sdk-integration-source="button-factory"></script>
-    @endenv
-
-    @env('staging')
-    <script
-        src="https://www.paypal.com/sdk/js?client-id=AZKXMPfJscqaryDzTCEnfpzP7CUT6rXYvS6EdQiX2FkCcSodMhqjYBmgBZvJLbRLonXetJ4BQClbYsJM&enable-funding=venmo&currency=USD"
-        data-sdk-integration-source="button-factory"></script>
-    @endenv
-
+    
     @routes
     <link rel="stylesheet" href="{{ url(mix('css/app.css')) }}">
     <script src="{{ url(mix('js/app.js')) }}" defer></script>
